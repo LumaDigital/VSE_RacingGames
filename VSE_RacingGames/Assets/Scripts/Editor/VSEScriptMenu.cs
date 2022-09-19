294e2b5fe4f7e0cebc3a7fc162f2a50c8853094d
DiwajKh9n <jawidkhan96@gmail.com>
2022-09-19 13:09:17 +0200

    [VSE] Menu tool for script handling

---
+++ VSE_RacingGames/Assets/Scripts/Editor/VSEScriptMenu.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEditor;
+using UnityEngine.Splines;
+
+public class VSEScriptMenu
+{
+    [MenuItem("VSE/Add Components/Spline Tools/Spline Gizmo Display")]
+    public static void AddSplineDisplayComponent()
+    {
+        AddComponentToSelectedObject(typeof(SplineDisplay), typeof(SplineContainer));
+    }
+
+    [MenuItem("VSE/Add Components/Spline Tools/Spline Length Display")]
+    public static void AddSplineLengthComponent()
+    {
+        AddComponentToSelectedObject(typeof(SplineLength), typeof(SplineContainer));
+    }
+
+    [MenuItem("VSE/Add Components/Racer's Progress Display")]
+    public static void AddHorseDistanceComponent()
+    {
+        AddComponentToSelectedObject(typeof(HorseDistance), typeof(SplineAnimate));
+    }
+
+    static void AddComponentToSelectedObject(Type componentToAdd, Type requiredComponent = null)
+    {
+        if (Selection.activeGameObject == null)
+        {
+            EditorUtility.DisplayDialog("No object selected!", "Please select an object before adding Tool!", "Okay");
+            return;
+        }
+
+        if (requiredComponent != null && Selection.activeGameObject.GetComponent(requiredComponent) == null)
+        {
+            if (DisplayMissingComponentDialog(requiredComponent))
+                Selection.activeGameObject.AddComponent(requiredComponent);
+            else
+                return;
+        }
+
+        Selection.activeGameObject.AddComponent(componentToAdd);
+    }
+
+    static bool DisplayMissingComponentDialog(Type missingComponent)
+    {
+        return EditorUtility.DisplayDialog("Failed to add Tool to selected object!", "Tool requires the selected object " +
+            "to contain necessary component: " + missingComponent, "Add Missing Component", "Cancel");
+    }
+}
